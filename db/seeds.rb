bb3acd73fa0631fd609a529d3509aa96b19225bf
Sebastian Lozano <slozano@forwardair.com>
2024-08-26 18:20:50 +0100

    Add seeds for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+BLOG_SEED = 10
+SKILL_SEED = 5
+PORTFOLIO_SEED= 9
+
+BLOG_SEED.times do |itBlog|
+    Blog.create!(
+        title: "My blog #{itBlog}",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quod, sint ratione ex repellat tempora excepturi nesciunt iure ducimus, explicabo pariatur dolorem nemo animi officia at porro? Porro, excepturi ipsa!"
+    )
+end
+
+puts "#{BLOG_SEED} blogs created"
+
+SKILL_SEED.times do |itSkill|
+    Skill.create!(
+        title: "Rails #{itSkill}",
+        percent_used: 16
+    )
+end
+
+puts "#{SKILL_SEED} skills created"
+
+PORTFOLIO_SEED.times do |itPortfolio|
+    Portfolio.create!(
+        title:"Portfolio title #{itPortfolio}",
+        subtitle: "Portfolio subtitle #{itPortfolio}",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quod, sint ratione ex repellat tempora excepturi nesciunt iure ducimus, explicabo pariatur dolorem nemo animi officia at porro? Porro, excepturi ipsa!",
+        main_image: "https://via.placeholder.com/600x400",
+        thumb_image: "https://via.placeholder.com/350x200",
+    )
+end
+
+puts "#{PORTFOLIO_SEED} portfolio items created"
