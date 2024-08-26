9cdae43beb4f5e4c62ec71b9d1dfefa916a120ae
Sebastian Lozano <slozano@forwardair.com>
2024-08-26 19:16:54 +0100

    Add show action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,11 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: %i[edit update]
+  before_action :set_portfolio_item, only: %i[show edit update]
 
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  def show
   end
 
   def new
