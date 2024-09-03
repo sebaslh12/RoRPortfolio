fcecb78b27205ddbb4c20f1f712f11f2fe7616a7
Sebastian Lozano <slozano@forwardair.com>
2024-09-03 22:13:17 +0100

    Add status
    Add status enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_03_205235) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_03_210820) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
