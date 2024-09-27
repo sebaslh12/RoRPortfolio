3dc6cf28d1071ffef2878854a452b4859abdc9bd
Sebastian Lozano <slozano@forwardair.com>
2024-09-27 19:37:47 +0100

    Add custom route for toggle status

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
