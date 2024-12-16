67eaed2a5042eef329a8a36409d6bce32d4cb119
Sebastian Lozano <slozano@forwardair.com>
2024-12-16 11:46:21 +0000

    Set default values with callback

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,13 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle:'Ruby on Rails') }
+
+  # Active Record callback, called when doing a new or loading record
+  after_initialize :set_defaults
+
+  def set_defaults
+    # ||= only sets the value if it is nil
+    self.main_image ||= 'https://via.placeholder.com/600x400'
+    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+  end
 end
