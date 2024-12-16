d3f1c5129ba79ba44ac7626e92f40678273e99c2
Sebastian Lozano <slozano@forwardair.com>
2024-12-16 10:19:00 +0000

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,19 @@
 BLOG_SEED = 10
 SKILL_SEED = 5
-PORTFOLIO_SEED= 9
+PORTFOLIO_SEED = 9
+TOPIC_SEED = 3
+
+TOPIC_SEED.times do |itTopic|
+    Topic.create!(title:  "Topic #{itTopic}")
+end
+
+puts "#{TOPIC_SEED} topics created"
 
 BLOG_SEED.times do |itBlog|
     Blog.create!(
         title: "My blog #{itBlog}",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quod, sint ratione ex repellat tempora excepturi nesciunt iure ducimus, explicabo pariatur dolorem nemo animi officia at porro? Porro, excepturi ipsa!"
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quod, sint ratione ex repellat tempora excepturi nesciunt iure ducimus, explicabo pariatur dolorem nemo animi officia at porro? Porro, excepturi ipsa!",
+        topic_id: Topic.last.id
     )
 end
 
@@ -20,14 +28,23 @@ end
 
 puts "#{SKILL_SEED} skills created"
 
-PORTFOLIO_SEED.times do |itPortfolio|
+(PORTFOLIO_SEED - 1).times do |itPortfolio|
     Portfolio.create!(
         title:"Portfolio title #{itPortfolio}",
-        subtitle: "Portfolio subtitle #{itPortfolio}",
+        subtitle: "Ruby on Rails",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quod, sint ratione ex repellat tempora excepturi nesciunt iure ducimus, explicabo pariatur dolorem nemo animi officia at porro? Porro, excepturi ipsa!",
         main_image: "https://via.placeholder.com/600x400",
         thumb_image: "https://via.placeholder.com/350x200",
     )
 end
+
+Portfolio.create!(
+        title:"Portfolio title 8",
+        subtitle: "Angular",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quod, sint ratione ex repellat tempora excepturi nesciunt iure ducimus, explicabo pariatur dolorem nemo animi officia at porro? Porro, excepturi ipsa!",
+        main_image: "https://via.placeholder.com/600x400",
+        thumb_image: "https://via.placeholder.com/350x200",
+    )
+
 
 puts "#{PORTFOLIO_SEED} portfolio items created"
