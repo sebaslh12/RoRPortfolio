fc42aa89b9177066db28471c42289510a16b0ca7
Sebastian Lozano <slozano@forwardair.com>
2024-12-16 12:21:30 +0000

    Add technologies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_16_115727) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_16_121132) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -54,6 +54,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_16_115727) do
     t.text "badge"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.integer "portfolio_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -61,4 +69,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_16_115727) do
   end
 
   add_foreign_key "blogs", "topics"
+  add_foreign_key "technologies", "portfolios"
 end
