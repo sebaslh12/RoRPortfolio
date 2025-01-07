0a87a6d564206a83dfa9b40dc3d63159b7f60a18
Sebastian Lozano <sebaslh12@gmail.com>
2025-01-07 12:25:12 +0000

    Add login helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, data: {turbo_method: :delete}
+    else
+      [
+        link_to("Register", new_user_registration_path),
+        link_to("Login", new_user_session_path)
+      ].join(content_tag(:br)).html_safe
+    end
+  end
 end
