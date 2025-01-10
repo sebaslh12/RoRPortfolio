b83c559dc24f0c17ebd3f97674cb7cb55d13a8b8
Sebastian Lozano <sebaslh12@gmail.com>
2025-01-10 16:58:18 +0000

    Update validation to work using GuestUser class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, data: {turbo_method: :delete}
-    else
+    if current_user.is_a?(GuestUser)
       [
         link_to("Register", new_user_registration_path),
         link_to("Login", new_user_session_path)
       ].join(content_tag(:br)).html_safe
+    else
+      link_to "Logout", destroy_user_session_path, data: {turbo_method: :delete}
     end
   end
 
