b7857fa68e47a4190dc34be0d8bdb642a9efdddb
Sebastian Lozano <sebaslh12@gmail.com>
2025-01-21 16:39:20 +0000

    Rename blog layout

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy toggle_status ]
-  layout 'blogs'
+  layout 'blog'
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   # GET /blogs or /blogs.json
